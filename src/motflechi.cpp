e1f078b3af0a2d2ac9bbf77348d4b2e6da408d5c
Yves Ouvrard, alias Collatinus <yves.ouvrard@collatinus.org>
2018-05-16 11:43:40 +0200

    supp. commentaire

--- src/motflechi.cpp
+++ src/motflechi.cpp
@@ -701,12 +701,6 @@ QString MotFlechi::trGroupe(Requete* rtest, QString morph)
                     else fv.prepend("ne ");
                     lret.replace(lret.count()-1, fv);
                 }
-                /*
-                QString trs = r->trSub();
-                // quelques transformations
-                trs.replace(QRegularExpression("(^| )(me|te|la|le|se|les) (je|tu|il|elle|on|nous|vous|ils|elles) "),
-                                               "\\1\\3 \\2 ");
-                */
                 lret.append(r->trSub());
             }
         }
