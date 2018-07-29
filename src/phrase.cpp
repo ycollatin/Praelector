44b9d91ce95576d691d0fc98ecec33a10d69a365
Yves Ouvrard, alias Collatinus <yves.ouvrard@collatinus.org>
2018-07-29 09:22:25 +0200

    fixme, todo

--- src/phrase.cpp
+++ src/phrase.cpp
@@ -21,6 +21,7 @@
 // bin/corpus/phrases.txt
 
 //                          FIXME
+//        - caederem te nisi irascerer : l'édition de la tr de irascerer n'est pas enregistrée.
 //        - /ira furor breuis/ : Il semble que la relecture, en cas de choix autre que celui qui
 //          est surligné, se met à surligner plusieurs autres choix.
 //        - magnorum montium proceritas : on peut antéposer ante par tr. suiv., mais le déterminant
@@ -31,6 +32,7 @@
 //        - Échec pour les phrases longues : Socrates quidem dicebat multos homines, etc.
 //
 //                           TODO
+//          mettre correctement irascerer à la f. négative.
 //        - Possibilité de changer la place du groupe subordonné courant.
 //        - vérifier la mise à jour des requêtes proposées après validation
 //          de l'une d'entre elles.
