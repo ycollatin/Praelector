f201bf05c9f93eee203f6ce2c6275ac8a4e45178
Yves Ouvrard, alias Collatinus <yves.ouvrard@collatinus.org>
2018-10-13 09:45:51 +0200

    fixme

--- src/phrase.cpp
+++ src/phrase.cpp
@@ -21,6 +21,8 @@
 // bin/corpus/phrases.txt
 
 //                          FIXME
+//        - nulla res gerebatur : la nég1 française est placée après aucune
+//          au lieu d'être placée devant l'auxiliaire
 //        - motflechi.cpp : elidefr(), contraction à le : exclure "là le".
 //        - L'ablatif absolu postposé n'est pas proposé, même après validation. epitheteV ?
 //        - /ira furor breuis/ : Il semble que la relecture, en cas de choix autre que celui qui
